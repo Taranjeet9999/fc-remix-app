721dca18a0c7b3091a74ff8e375ff05da78145c7
Taranjeet <taranjeet@techformation.co.in>
2024-05-20 18:06:17 +0530

    bug fixes

--- web/frontend/components/newOrders/NewOrders.jsx
+++ web/frontend/components/newOrders/NewOrders.jsx
@@ -183,6 +183,8 @@ export function NewOrders(props) {
         );
         return { ...item1, ...matchingItem2 };
       });
+
+      
       setOrders(getOrders);
       setallNewOrders(getOrders);
       setIsLoading(false);
